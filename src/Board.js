4ddf79c4e43da972bf3d1078bb50fae359c815d6
kishan <kishansarvaiya9988@gmail.com>
2020-06-19 15:30:00 -0700

    checkerd game play
    play game

--- src/Board.js
+++ src/Board.js
@@ -18,7 +18,7 @@ export default class Board extends React.Component {
             <Square
                 key = {coordinates}
                 squareClasses = {squareClasses}
-                //onClick = {() => this.props.onClick(coordinates) }
+                onClick = {() => this.props.onClick(coordinates) }
             />
         );
     }
@@ -27,6 +27,7 @@ export default class Board extends React.Component {
         let boardRender = [];
         let columnsRender = [];
 
+        const moves = this.props.moves;
 
         for (let coordinates in this.props.boardState) {
 
@@ -37,6 +38,7 @@ export default class Board extends React.Component {
             const col = utils.getColAsInt(this.props.columns, coordinates);
             const row = utils.getRowAsInt(coordinates);
 
+            const currentPlayer = utils.returnPlayerName(this.props.currentPlayer);
 
             const colorClass  = ( (utils.isOdd(col) && utils.isOdd(row)) || (!utils.isOdd(col) && !(utils.isOdd(row)) ) ) ? 'white' : 'black';
 
@@ -45,6 +47,22 @@ export default class Board extends React.Component {
             squareClasses.push(coordinates);
             squareClasses.push(colorClass);
 
+            if (this.props.activePiece === coordinates) {
+                squareClasses.push('isActive');
+            }
+
+            if (moves.indexOf(coordinates) > -1) {
+                let moveClass = 'movable ' + currentPlayer + '-move';
+                squareClasses.push(moveClass);
+            }
+
+            if (this.props.boardState[coordinates] !== null) {
+                squareClasses.push(this.props.boardState[coordinates].player + ' piece');
+
+                if (this.props.boardState[coordinates].isKing === true ) {
+                    squareClasses.push('king');
+                }
+            }
 
             squareClasses = squareClasses.join(' ');
 
